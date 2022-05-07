a96614e075b6873900f8ee80e8b7637a3fcffce5
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-05-07 14:54:53 +0530

    Integration Tests

--- src/util/numbers.test.js
+++ src/util/numbers.test.js
@@ -1,30 +1,43 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from "vitest";
 
-import { transformToNumber } from './numbers';
+import { transformToNumber, cleanNumbers } from "./numbers";
 
-it('should transform a string number to a number of type number', () => {
-  const input = '1';
+describe("transformToNumber()", () => {
+  it("should transform a string number to a number of type number", () => {
+    const input = "1";
 
-  const result = transformToNumber(input);
+    const result = transformToNumber(input);
 
-  expect(result).toBeTypeOf('number');
+    expect(result).toBeTypeOf("number");
+  });
+
+  it("should transform a string number to a number of type number", () => {
+    const input = "1";
+
+    const result = transformToNumber(input);
+
+    expect(result).toBe(+input);
+  });
+
+  it("should yield NaN for non-transformable values", () => {
+    const input = "invalid";
+    const input2 = {};
+
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+  });
 });
 
-it('should transform a string number to a number of type number', () => {
-  const input = '1';
+describe("cleanNumbers", () => {
+  it("should return an array of numbers if an array of string numbers are provided", () => {
+    const numbers = ["1", "2"];
 
-  const result = transformToNumber(input);
+    const result = cleanNumbers(numbers);
 
-  expect(result).toBe(+input);
-});
-
-it('should yield NaN for non-transformable values', () => {
-  const input = 'invalid';
-  const input2 = {};
-
-  const result = transformToNumber(input);
-  const result2 = transformToNumber(input2);
-
-  expect(result).toBeNaN();
-  expect(result2).toBeNaN();
+    const expected = [1, 2];
+    expect(result[0]).toBeTypeOf("number");
+  });
 });
