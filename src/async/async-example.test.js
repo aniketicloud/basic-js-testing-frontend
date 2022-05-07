e02270a3afa8be4ec1cde9e90e742e7fc1676689
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-05-07 23:20:10 +0530

    testing async code with callbacks

--- src/async/async-example.test.js
+++ src/async/async-example.test.js
@@ -5,10 +5,10 @@ it("shoud generate a token value", (done) => {
   const testUserEmail = "test@test.com";
 
   const token = generateToken(testUserEmail, (error, token) => {
-    expect(token).toBeDefined();
     // expect(token).toBe(2); // this test is also passed even though th test should fail
     try {
-      expect(token).toBe(2);
+      // expect(token).toBe(2);
+      expect(token).toBeDefined();
       done();
     } catch (error) {
       done(error);
