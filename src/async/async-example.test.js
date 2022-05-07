d807067a6661efb2ba2dff8c3ff51960a2409657
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-05-07 23:18:41 +0530

    solved issue of timeput failure

--- src/async/async-example.test.js
+++ src/async/async-example.test.js
@@ -1,10 +1,17 @@
 import { it, expect } from "vitest";
 import { generateToken } from "./async-example";
 
-it("shoud generate a token value", () => {
+it("shoud generate a token value", (done) => {
   const testUserEmail = "test@test.com";
 
   const token = generateToken(testUserEmail, (error, token) => {
-    expect(token).toBe(2); // this test is also passed even though th test should fail 
+    expect(token).toBeDefined();
+    // expect(token).toBe(2); // this test is also passed even though th test should fail
+    try {
+      expect(token).toBe(2);
+      done();
+    } catch (error) {
+      done(error);
+    }
   });
 });
